e752b83d3131289263d0493287dd2c20336a56b6
Adam Badar <adam.badar@mail.utoronto.ca>
2023-08-25 08:27:56 -0400

    feat: :sparkles: created basic buttons for block and unblock

--- work-x/src/popup.tsx
+++ work-x/src/popup.tsx
@@ -3,9 +3,29 @@ import { CountButton } from "~features/count-button"
 import "~style.css"
 
 function IndexPopup() {
+  const handleBlockWebsite = () => {
+    console.log("handleBlockWebsite");
+  }
+
+  const handleUnblockWebsite = () => {
+    console.log("handleUnblockWebsite");
+  }
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
+    <div className="w-80 h-96 bg-zinc-300 flex flex-col justify-center items-center">
+      <div
+        className="w-36 h-10 px-4 py-2 bg-zinc-600 rounded"
+        onClick={handleBlockWebsite}>
+        <div className="text-center text-white text-base font-normal leading-normal">
+          Block Website
+        </div>
+      </div>
+      <div
+        className="w-40 h-10 mt-2 px-4 py-2 bg-zinc-600 rounded"
+        onClick={handleUnblockWebsite}>
+        <div className="text-center text-white text-base font-normal leading-normal">
+          Unblock Website
+        </div>
+      </div>
     </div>
   )
 }
